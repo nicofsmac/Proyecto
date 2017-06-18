efe38dc39fe258d2a5672ef35de6ad503d5852d1
Guillermo Romeo Estevez <romeoestevez@gmail.com>
2017-06-18 18:14:59 -0300

    Registro completo !

--- registroPOO.php
+++ registroPOO.php
@@ -73,25 +73,59 @@ foreach($errores as $e) {
 }
 ?>
 </div>
+<!-- Viejo form  -->
 
-<form method="post">
-
+<!-- <form method="post">
 <label>Usuario:</label>
 <input type="text" name="usuario">
 <br>
-
 <label>Email:</label>
 <input type="text" name="email">
 <br>
-
 <label>Password:</label>
 <input type="password" name="password">
 <br>
-
 <input type="submit" name="enviar" value="enviar">
 
 </form>
-</div>
+</div> -->
+
+<!-- Formulario de bootstrap  -->
+<form class="form-horizontal" method="post">
+	<div class="form-group">
+		<label for="inputEmail3" class="col-sm-2 control-label">Email:</label>
+		<div class="col-sm-10">
+			<input type="email" class="form-control" id="inputEmail3"  name="email">
+		</div>
+	</div>
+	<div class="form-group">
+    <label for="inputUsuario" class="col-sm-2 control-label">Usuario:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputUsuario"  name="usuario">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Contraseña:</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3"  name="password">
+    </div>
+  </div>
+  <!-- <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Remember me
+        </label>
+      </div>
+    </div>
+  </div> -->
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default" name="enviar" value="enviar">Registraté</button>
+    </div>
+  </div>
+</form>
+
 <?php
 include "footer.php"
 ?>
